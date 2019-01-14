95e82348668c67c6fc9dbfe634c22366a594a956
Ed Maphis <emaphis85@gmail.com>
2019-01-14 11:09:02 -0500

    hangman project

---
+++ hangman/project.clj
@@ -0,0 +1,7 @@
+(defproject wonderland "0.1.0-SNAPSHOT"
+  :description "Hangman Game"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/test.check "0.9.0"]])
