e75b65ce7c2eb32516c09818061a1bbfd134369f
Ed Maphis <emaphis85@gmail.com>
2019-01-17 23:57:03 -0500

    Functional programming

--- programming-clojure/src/programming_clojure/ch02-functional.clj
+++ programming-clojure/src/programming_clojure/ch02-functional.clj
@@ -2,12 +2,20 @@
   (:require [clojure.repl :refer :all]
             [clojure.edn :as edn]))
 
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; What does functional programming even mean.
+;;  * Preference for working with immutable `values`:
+;;    - Use of immutable data-structures that satisfy simple abstractions
+;;    - Functions are first class values.
+;;  * Declarative processing of data over imperative control structures
+;;  * Natural, incremental composition of functions, higher-order functions
+;;    immutable data-structures.
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Comparing Values to Mutable Objects
 
+;; see src/java/Statefulinteger.java
 ;; lein javac
 
 (import StatefulInteger)
@@ -23,7 +31,7 @@
 ;; => 5.0
 (= five six)
 ;; => false
-(= five five)
+(= five 5)
 ;; => true
 (.setInt five 6)
 (= five six)
@@ -43,6 +51,7 @@
 (= five (StatefulInteger. 42))
 ;; => true
 
+;; Mutable object can lead to strange results.
 
 ;;; the safety of values.
 (def h {[1,2] 3})
@@ -66,12 +75,13 @@ h
 ;;  * Copy constructors and deep copy methods to ensure reliable access to
 ;;    objects in a particular state.
 ;;  * patterns for tracking and managing change over time, including Observer,
-;;    Reactor ...
+;;     Reactor ...
 ;;  * Utilities for erecting flimsy immutable facades in front of mutable
-;;    datastructures.
+;;    data-structures.
 ;;  * Documentation and questionable advice. - deadlocks, race conditions.
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; First-Class and Higher-Order Functions
 ;;  Functions as datatypes.
 
@@ -129,3 +139,222 @@ h
 
 ;; reduce a collection of numbers into a `map`, with numbers for keys, and
 ;; their squares for values:
+(reduce
+ (fn [mp vc]
+   (assoc mp vc (* vc vc)))
+ {}
+ [1 2 3 4])
+;; => {1 1, 2 4, 3 9, 4 16}
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Applying Ourselves Partially
+
+;; `Function application` is the invocation of a function with a sequence
+;; of arguments rather then using syntactical invocation.
+
+;; function application by `apply`.
+
+(apply hash-map [:a 5 :b 6])
+;; => {:b 6, :a 5}
+
+(def args [2 -2 10])
+(apply * 0.5 3 args)
+;; => -60.0
+
+(def only-strings (partial filter string?))
+(only-strings ["a" 5 "b" 6])
+;; => ("a" "b")
+
+(def add-3 (partial + 3))
+(add-3 4)
+;; => 7
+
+;; `partial` versus function literals.
+
+(#(filter string? %) ["a" 5 "b" 6])
+;; => ("a" "b")
+
+;; However, function literals do not limit you to defining only the
+;; initial arguments to the function.
+
+(#(filter % ["a" 5 "b" 6]) string?)
+;; => ("a" "b")
+
+(#(filter % ["a" 5 "b" 6]) number?)
+;; => (5 6)
+
+;; But you are forced to fully specify the arguments.
+
+;; (#(map *) [1 2 3] [4 5 6] [7 8 9])
+;; 1. Unhandled clojure.lang.ArityException
+;; Wrong number of args (3) passed to:
+
+(#(map * % %2 %3) [1 2 3] [4 5 6] [7 8 9])
+;; => (28 80 162)
+
+;; (#(map * % %2 %3) [1 2 3] [4 5 6])
+;; 1. Unhandled clojure.lang.ArityException
+;; Wrong number of args (2) passed to:
+
+(#(apply map * %&) [1 2 3] [4 5 6] [7 8 9])
+;; => (28 80 162)
+
+(#(apply map * %&) [1 2 3])
+;; => (1 2 3)
+
+((partial map * ) [1 2 3] [4 5 6] [7 8 9])
+;; => (28 80 162)
+
+((partial #(filter string? %)) ["a" 5 "b" 6])
+;; => ("a" "b")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Composition of Function(ality)
+
+;; A function of a negation of a sum of given numbers returned
+;; as a string.
+(defn negated-sum-str
+  [& numbers]
+  (str (- (apply + numbers))))
+
+(negated-sum-str 10 12 3.4)
+;; => "-25.4"
+
+;; `comp` used for clarity.
+(def negated-sum-str (comp str - +))
+
+(negated-sum-str 10 12 3.4)
+;; => "-25.4"
+
+;; but functions must be composed in correct order for types.
+
+;; ((comp + - str) 5 10)
+;; 1. Unhandled java.lang.ClassCastException
+;; class java.lang.String cannot be cast to class java.lang.Number
+
+;; CamelCase to keyword converter.
+
+(require '[clojure.string :as str])
+
+(def camel->keyword (comp keyword
+                          str/join
+                          (partial interpose \-)
+                          (partial map str/lower-case)
+                          #(str/split % #"(?<=[a-z])(?=[A-Z])")))
+
+(camel->keyword "CamelCase")
+;; => :camel-case
+(camel->keyword "lowerCamelCase")
+;; => :lower-camel-case
+
+;; More composition: Returns an idiomatic Clojure map given as sequence
+;; of key/value pairs that use CamelCase-style keys
+
+(def camel-pairs->map
+  (comp (partial apply hash-map)
+        (partial map-indexed (fn [i x]
+                               (if (odd? i)
+                                 x
+                                 (camel->keyword x))))))
+
+(camel-pairs->map ["CamelCase" 5 "lowerCamelCase" 3])
+;; => {:camel-case 5, :lower-camel-case 3}
+
+
+;;; writing  Higher-Order Functions
+
+;; A HOF that returns a function that adds a given number to its argument
+(defn adder
+  [n]
+  (fn [x] (+ n x)))
+
+((adder 5) 18)
+;; => 23
+
+(def add-3 (adder 3))
+
+(add-3 4) ;; => 7
+
+;; A HOF that returns a function that doubles the result of calling the
+;; provided function
+(defn doubler
+  [f]
+  (fn [& args]
+    (* 2 (apply f args))))
+
+(def double-+ (doubler +))
+
+(double-+ 1 2 3)
+;; => 12
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Building a Primitive Logging System with
+;;; Composable Higher-Order Functions
+
+(defn print-logger
+  [writer]
+  #(binding [*out* writer]
+     (println %)))
+
+(def *out*-logger (print-logger *out*))
+
+(*out*-logger "hello")
+;; hello
+;; => nil
+
+;; create and retain a StringWriter.
+(def writer (java.io.StringWriter.))
+(def retained-logger (print-logger writer))
+
+;; Calling our logging function doesn't write to stdout.
+(retained-logger "hello")
+;; => nil
+
+;;... because it's been println'ed to out StringWriter instead.
+(str writer)
+;; => "hello\r\n"
+
+(require 'clojure.java.io)
+
+(defn file-logger
+  [file]
+  #(with-open [f (clojure.java.io/writer file :append true)]
+     ((print-logger f) %)))
+
+;; Let's see how we're doing:
+(def log->file (file-logger "messages.log"))
+;; => #'programming-clojure.ch02-functional/log->file
+
+(log->file "hello log")
+;; => nil
+
+;; logging to multiple destinations.
+(defn multi-logger
+  [& logger-fns]
+  #(doseq [f logger-fns]
+     (f %)))
+
+(def log (multi-logger
+          (print-logger *out*)
+          (file-logger "messages.log")))
+
+(log "hello log again")
+;; "hello log again
+;; => nil
+
+;; logging middle-ware: include a `time-stamp` with each log message
+
+(defn timestamped-logger
+  [logger]
+  #(logger (format "[%1$tY-%1$tm-%1$te %1$tH:%1$tM:%1$tS] %2$s" (java.util.Date.) %)))
+
+(def log-timestamped (timestamped-logger
+                      (multi-logger
+                       (print-logger *out*)
+                       (file-logger "messages.log"))))
+
+(log-timestamped "goodbye log, new")
+;; [2019-01-17 23:54:37] goodbye log, new
+;; => nil
