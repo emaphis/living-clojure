982c9371f48013672d613879b571dea4bcca6596
Ed Maphis <emaphis85@gmail.com>
2019-08-21 20:57:50 -0400

    Add event handling

--- cheshire-cat/src-cljs/core.cljs
+++ cheshire-cat/src-cljs/core.cljs
@@ -3,7 +3,15 @@
   (:require [clojure.browser.repl :as repl]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
-            [enfocus.core :as ef]))
+            [enfocus.core :as ef]
+            [enfocus.events :as ev]
+            [enfocus.effects :as ee]))
+
+(defn say-goodbye []
+  (ef/at
+   "#cat-name" (ee/fade-out 500)
+   "#button1" (ee/fade-out 500)
+   "#status" (ee/fade-out 5000)))
 
 (defn ^:export init []
   (repl/connect "http://localhost:9000/repl")
@@ -13,5 +21,6 @@
                "#status" (ef/do->
                           (ef/content (:status body))
                           (ef/set-style :font-size "500%")))
-        )))
-
+        (ef/at "button1" (ev/listen
+                          :click
+                          #(js/alert "bye!"))))))
