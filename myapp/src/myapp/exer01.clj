6a06cca4fcbbca8755080744be72ffd70a8a104d
Ed Maphis <emaphis85@gmail.com>
2019-12-13 15:01:31 -0500

    Day 3 exercises.

--- myapp/src/myapp/exer01.clj
+++ myapp/src/myapp/exer01.clj
@@ -216,6 +216,100 @@
 
 (deftest test36
   (testing "Let it Be"
-    (is (= 10 (let [x 7 y 3 z 1] (+ x y))))
-    (is (= 4 (let [x 7 y 3 z 1] (+ y z))))
-    (is (= 1 (let [x 7 y 3 z 1] z)))))
+    (is (= 10 (let [x 7 y 3 _ 1] (+ x y))))
+    (is (= 4 (let [_ 7 y 3 z 1] (+ y z))))
+    (is (= 1 (let [_ 7 _ 3 z 1] z)))))
+
+
+;;; Ex 037 -  Elementary.
+;;  Regex patterns are supported with a special reader macro.
+
+(def str37 "ABC")
+(deftest test37
+  (testing "Regular Expressions")
+  (is (= str37 (apply str (re-seq #"[A-Z]+" "bA1B3Ce ")))))
+
+
+;;; Ex 057 Elementary
+;;  A recursive function is a function which calls itself. This is one of the
+;;  fundamental techniques used in functional programming.
+
+(def lst57 '(5 4 3 2 1))
+
+(deftest test57
+  (testing "Simple Recursion"
+    (is (= lst57 ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5)))))
+
+
+;;; Ex 068 - Elementary - recursion
+;;  Clojure only has one non-stack-consuming looping construct: recur.
+;;  Either a function or a loop can be used as the recursion point.
+;;  Either way, recur rebinds the bindings of the recursion point to the values
+;;  it is passed.
+;;  Recur must be called from the tail-position, and calling it elsewhere will
+;;  result in an error.
+
+(def lst68 '(7 6 5 4 3))
+
+(deftest test68
+  (testing "Recurring Theme"
+    (is (= lst68
+           (loop [x 5
+                  result []]
+             (if (> x 0)
+               (recur (dec x) (conj result (+ 2 x)))
+               result))))))
+
+
+;;; Ex 071 - Elementary
+;;  The -> macro threads an expression x through a variable number of forms.
+;;  First, x is inserted as the second item in the first form, making a list of
+;;  it if it is not a list already.
+;;  Then the first form is inserted as the second item in the second form,
+;;  making a list of that form if necessary. This process continues for all the
+;;  forms. Using -> can sometimes make your code more readable.
+
+;; last
+(deftest test71
+  (testing "Rearranging Code: ->"
+    (is (= (last (sort (rest (reverse [2 5 4 1 3 6]))))
+           (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (last))
+           5))))
+
+
+
+;;; Ex 072 - Elementary
+;; The ->> macro threads an expression x through a variable number of forms.
+;; First, x is inserted as the last item in the first form, making a list of
+;; it if it is not a list already. Then the first form is inserted as the last
+;; item in the second form, making a list of that form if necessary.
+;; This process continues for all the forms. Using ->> can sometimes make your
+;; code more readable.
+
+(deftest test72
+  (testing "Rearranging Code: ->>"
+    (is (= (reduce + (map inc (take 3 (drop 2 [2 5 4 1 3 6]))))
+           (->> [2 5 4 1 3 6] (drop 2) (take 3) (map inc) (reduce +))
+           11))))
+
+
+;;; Ex 145 - Elementary - core-functions - seqs
+;;  Clojure's for macro is a tremendously versatile mechanism for producing a
+;;  sequence based on some other sequence(s). It can take some time to
+;;  understand how to use it properly, but that investment will be paid back
+;;  with clear, concise sequence-wrangling later. With that in mind, read over
+;;  these for expressions and try to see how each of them produces the same
+;;  result.
+
+(def lst145 '(1 5 9 13 17 21 25 29 33 37))
+(deftest test 145
+  (testing "For the win"
+    (is (= lst145 (for [x (range 40)
+                        :when (= 1 (rem x 4))]
+                    x)))
+    (is (= lst145 (for [x (iterate #(+ 4 %) 0)
+                        :let [z (inc x)]
+                        :while (< z 40)]
+                    z)))
+    (is (= lst145 (for [[x y] (partition 2 (range 20))]
+                    (+ x y))))))
