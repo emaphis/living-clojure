a462455b8f8c0f7bb2d7485799542a74d5307084
Ed Maphis <emaphis85@gmail.com>
2020-12-18 21:22:36 -0500

    More multimethods.

--- workshop/workshop/src/workshop/chap03.clj
+++ workshop/workshop/src/workshop/chap03.clj
@@ -582,3 +582,62 @@
 (move {:position {:x 10 :y 10 :facing :wall}})
 ;; => {:position {:x 10, :y 10, :facing :wall}}
 
+
+;;; Activity 3.01: Building a Distance and Cost Calculator
+;;  Back to flight Booking - pg 118
+
+
+;; #1
+(def walking-speed 4)
+(def driving-speed 70)
+
+;; #2
+(def paris {:lat 48.856483 :lon 2.352413})
+(def bordeaux {:lat 44.834999 :lon -0.575490})
+(def london {:lat 51.507351, :lon -0.127758})
+(def manchester {:lat 53.480759, :lon -2.242631})
+
+;; #6
+(def vehicle-cost-fns
+  {:sporche (partial * 0.12 1.3)
+   :tayato (partial * 0.07 1.3)
+   :sleta (partial * 0.2 0.1)})
+
+;; #3
+(defn distance
+  "Calculate the distance between to coordinate points"
+  [{lat1 :lat lon1 :lon} {lat2 :lat lon2 :lon}]
+  (let [x-dis (- lat2 lat1)
+        y-dis (* (Math/cos lat2) (- lon2 lon1))]
+    (* 110.25 (Math/sqrt (+ (* y-dis y-dis) (* x-dis x-dis))))))
+
+;; #4
+(defmulti itinerary
+  "Calculate the distance of travel between two location, and the cost and
+  duration based on the type of transportation"
+  :transport)
+
+;; #5
+(defmethod itinerary :walking
+  [{:keys [:from :to]}]
+  (let [walking-distance (distance from to)
+        duration (/ walking-distance walking-speed)]
+    {:cost 0 :distance walking-distance :duration duration}))
+
+;; #7
+(defmethod itinerary :driving
+  [{:keys [:from :to :vehicle]}]
+  (let [driving-distance (distance from to)
+        cost ((vehicle vehicle-cost-fns) driving-distance)
+        duration (/ driving-distance driving-speed)]
+    {:cost cost :distance driving-distance :duration duration}))
+
+
+(itinerary {:from paris :to bordeaux :transport :walking})
+;; => {:cost 0, :distance 491.61380776549225, :duration 122.90345194137306}
+(itinerary {:from paris :to bordeaux :transport :driving :vehicle :tayato})
+;; => {:cost 44.7368565066598, :distance 491.61380776549225, :duration 7.023054396649889}
+(itinerary {:from london :to manchester :transport :walking})
+;; => {:cost 0, :distance 318.4448148814284, :duration 79.6112037203571}
+(itinerary {:from manchester :to london :transport :driving :vehicle :sleta})
+;; => {:cost 4.604730845743489, :distance 230.2365422871744, :duration 3.2890934612453484}
