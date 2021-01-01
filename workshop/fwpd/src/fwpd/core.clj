37a4700a326029eb72a39410e19cf735d67d5f80
Ed Maphis <emaphis85@gmail.com>
2021-01-01 11:06:53 -0500

    Ok

--- workshop/fwpd/src/fwpd/core.clj
+++ workshop/fwpd/src/fwpd/core.clj
@@ -37,6 +37,27 @@
   [minimum-glitter records]
   (filter #(>= (:glitter-index %) minimum-glitter) records))
 
+;;; Exercises
+
+;; 1. Turn the result of your glitter filter into a list of names.
+
+(defn suspect-name-list
+  [suspects]
+  (map :name suspects))
+
+;; 2. Write a function, append, which will append a new suspect to your list of suspects.
+(defn append
+  [suspect records]
+  (conj suspect records))
+
+;; 3. Write a validator function
+;; validators is a map of fields to validation functions
+;; record is the record that needs validation
+(defn validate
+  [validators record]
+  )
+
+
 (comment
   (= 1000 (str->int "1000"))
   (= 3 (convert :glitter-index "3"))
@@ -44,4 +65,9 @@
   (mapify [["Bella Swan" 0]])
   (first (mapify (parse (slurp filename))))
   (glitter-filter 3 (mapify (parse (slurp filename))))
+  (suspect-name-list (glitter-filter 3 (mapify (parse (slurp filename)))))
+  (append (suspect-name-list (glitter-filter 3 (mapify (parse (slurp filename))))) {:name "Jenny" :glitter-index 10})
+  
+
+
   )
