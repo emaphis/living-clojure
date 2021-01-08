92b31ba4a99ab59a1ed27ab21ca95ae371b177d2
Ed Maphis <emaphis85@gmail.com>
2021-01-08 18:12:21 -0500

    pegthing user interface.

--- workshop/pegthing/src/pegthing/core.clj
+++ workshop/pegthing/src/pegthing/core.clj
@@ -1,28 +1,28 @@
 (ns pegthing.core
-  (:require [clojure.set :as set]
-            [clojure.string :as s])
+  (:require [clojure.string :as s])
   (:gen-class))
 
 (declare successful-move prompt-move game-over query-rows)
 
 ;;; 1. Creating a new board
-
-{1  {:pegged true, :connections {6 3, 4 2}},
- 2  {:pegged true, :connections {9 5, 7 4}},
- 3  {:pegged true, :connections {10 6, 8 5}},
- 4  {:pegged true, :connections {13 8, 11 7, 6 5, 1 2}},
- 5  {:pegged true, :connections {14 9, 12 8}},
- 6  {:pegged true, :connections {15 10, 13 9, 4 5, 1 3}},
- 7  {:pegged true, :connections {9 8, 2 4}},
- 8  {:pegged true, :connections {10 9, 3 5}},
- 9  {:pegged true, :connections {7 8, 2 5}},
- 10 {:pegged true, :connections {8 9, 3 6}},
- 11 {:pegged true, :connections {13 12, 4 7}},
- 12 {:pegged true, :connections {14 13, 5 8}},
- 13 {:pegged true, :connections {15 14, 11 12, 6 9, 4 8}},
- 14 {:pegged true, :connections {12 13, 5 9}},
- 15 {:pegged true, :connections {13 14, 6 10}},
- :rows 5}
+(comment
+  ;; example board
+  {1  {:pegged true, :connections {6 3, 4 2}},
+   2  {:pegged true, :connections {9 5, 7 4}},
+   3  {:pegged true, :connections {10 6, 8 5}},
+   4  {:pegged true, :connections {13 8, 11 7, 6 5, 1 2}},
+   5  {:pegged true, :connections {14 9, 12 8}},
+   6  {:pegged true, :connections {15 10, 13 9, 4 5, 1 3}},
+   7  {:pegged true, :connections {9 8, 2 4}},
+   8  {:pegged true, :connections {10 9, 3 5}},
+   9  {:pegged true, :connections {7 8, 2 5}},
+   10 {:pegged true, :connections {8 9, 3 6}},
+   11 {:pegged true, :connections {13 12, 4 7}},
+   12 {:pegged true, :connections {14 13, 5 8}},
+   13 {:pegged true, :connections {15 14, 11 12, 6 9, 4 8}},
+   14 {:pegged true, :connections {12 13, 5 9}},
+   15 {:pegged true, :connections {13 14, 6 10}},
+   :rows 5})
 
 (defn tri*
   "Generates lazy sequence of triangular numbers"
@@ -283,10 +283,86 @@
 ;;; 4. Handling user interaction
 ;;;    Player Interaction
 
+(defn letter->pos
+  "Convert a letter string to the corresponding position number"
+  [letter]
+  (inc (- (int (first letter)) alpha-start)))
 
+
+(defn get-input
+  "Waits for user to enter text and hit enter, then cleans the input"
+  ([] (get-input nil))
+  ([default]
+   (let [input (s/trim (read-line))]
+     (if (empty? input)
+       default
+       (s/lower-case input)))))
+
+
+(defn characters-as-strings
+  "Given a string, return a collection consisting of each individual
+  character"
+  [string]
+  (re-seq #"[a-zA-Z]" string))
+
+(declare user-entered-valid-move user-entered-invalid-move prompt-rows)
+
+(defn prompt-move
+  [board]
+  (println "\nHere's your board:")
+  (print-board board)
+  (println "Move from where to where? Enter two letters:")
+  (let [input (map letter->pos (characters-as-strings (get-input)))]
+    (if-let [new-board (make-move board (first input) (second input))]
+      (user-entered-valid-move new-board)
+      (user-entered-invalid-move board))))
+
+(defn user-entered-invalid-move
+  "Handles the next step after a user has entered an invalid move"
+  [board]
+  (println "\n!!! That was an invalid move :(n")
+  (prompt-move board))
+
+(defn user-entered-valid-move
+  "Handles the next step after a user has entered a valid move"
+  [board]
+  (if (can-move? board)
+    (prompt-move board)
+    (game-over board)))
+
+(defn game-over
+  "Announce the game is over and prompt to play again"
+  [board]
+  (let [remaining-pegs (count (filter :pegged (vals board)))]
+    (println "Game over! You had" remaining-pegs "pegs left:")
+    (print-board board)
+    (println "Play again? y/n [y]")
+    (let [input (get-input "y")]
+      (if (= "y" input)
+        (prompt-rows)
+        (do
+          (println "Bye!")
+          (System/exit 0))))))
+
+
+(defn prompt-empty-peg
+  [board]
+  (println "Here's your board:")
+  (print-board board)
+  (println "Remove which peg? [e]")
+  (prompt-move (remove-peg board (letter->pos (get-input "e")))))
+
+(defn prompt-rows
+  []
+  (println "How many rows? [5]")
+  (let [rows (Integer. (get-input 5))
+        board (new-board rows)]
+    (prompt-empty-peg board)))
+
+
+;;; Program entry point.
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Start the pegthing program"
   [& args]
-  (println "Hello, World!"))
-
-
+  (println "Get ready to play thing!")
+  (prompt-rows))
