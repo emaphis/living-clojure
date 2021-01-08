030cca81b01fa5ddecd5951ab53037a6380aaa00
Ed Maphis <emaphis85@gmail.com>
2021-01-07 23:33:34 -0500

    Functions to print the board.

--- workshop/pegthing/src/pegthing/core.clj
+++ workshop/pegthing/src/pegthing/core.clj
@@ -1,5 +1,6 @@
 (ns pegthing.core
-  (:require [clojure.set :as set])
+  (:require [clojure.set :as set]
+            [clojure.string :as s])
   (:gen-class))
 
 (declare successful-move prompt-move game-over query-rows)
@@ -216,8 +217,71 @@
 
 
 ;;; 3. Representing a board textually
+;;;    Rendering and Printing the Board
 
-;;; 4.  Handling user interaction
+(def alpha-start 97)
+(def alpha-end 123)
+(def letters (map (comp str char) (range alpha-start alpha-end)))
+(def pos-chars 3)
+
+(def ansi-style
+  {:red "[31m"
+   :green "[32m"
+   :blue "[34m"
+   :reset "[0m"})
+
+(defn ansi
+  "Returns string that will apply ANSI style"
+  [style]
+  (str "\u001b" (style ansi-style)))
+
+;; (defn ansi
+;;   "Produce a string which will apply an ansi style"
+;;   [style]
+;;   (if (< (.indexOf (System/getProperty "os.name") "Windows") 0)
+;;     (str \u001b (style ansi-styles))
+;;     ""))
+
+(defn colorize
+  "Apply ansi color to text"
+  [text color]
+  (str (ansi color) text (ansi :reset)))
+
+
+(defn render-pos
+  [board pos]
+  (str (nth letters (dec pos))
+       (if (get-in board [pos :pegged])
+         (colorize "0" :blue)
+         (colorize "_" :red))))
+
+
+(defn row-positions
+  "Return all positions in the given row"
+  [row-num]
+  (range (inc (or (row-tri (dec row-num)) 0))
+         (inc (row-tri row-num))))
+
+(defn row-padding
+  "String of spaces to add to the beggining of a row to center it"
+  [row-num rows]
+  (let [pad-length (/ (* (- rows row-num) pos-chars) 2)]
+    (apply str (take pad-length (repeat " ")))))
+
+(defn render-row
+  [board row-num]
+  (str (row-padding row-num (:rows board))
+       (s/join " " (map (partial render-pos board)
+                        (row-positions row-num)))))
+
+(defn print-board
+  [board]
+  (doseq [row-num (range 1 (inc (:rows board)))]
+    (println (render-row board row-num))))
+
+
+;;; 4. Handling user interaction
+;;;    Player Interaction
 
 
 (defn -main
